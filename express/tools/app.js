29f82b2e74b8b4ca4b078f46acf874d98ae69708
Kimi <sianyu@icloud.com>
2018-05-06 06:11:15 -0700

    add MongoDB/ETC tools

---
+++ express/tools/app.js
@@ -0,0 +1,103 @@
+
+Web3 = require('web3')
+var web3 = new Web3(new Web3.providers.HttpProvider("https://web3.gastracker.io"));
+
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+
+
+
+
+var getblock = function(para) {
+    return new Promise(function(resolve, reject) {
+
+        web3.eth.getBlock(para, function(error, result){
+            if(!error){
+                //console.log(JSON.stringify(result));
+                resolve(result);
+            }
+
+            else
+                console.error(error);
+        })
+
+    })
+}
+
+
+
+
+var sleep = function(para) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve(para * para)
+        }, 1000)
+    })
+}
+
+
+
+
+async function main() {
+
+
+    //var result1 = await getblock(5700000);
+    //console.log(result1.timestamp);
+
+    //var result1 = await getblock(5);
+    //console.log(result1.timestamp);
+    //var result2 = await getblock(4);
+    //console.log(result2.timestamp);
+    //console.log(parseInt(result1.timestamp)-parseInt(result2.timestamp));
+
+
+
+    var count =0
+    var sum =0;
+    for (var i=5000000;i< 6000000; i += 1000)
+    {
+        var result1 = await getblock(i);
+        //console.log(result1.difficulty.toString());
+        var result2 = await getblock(i-1);
+        //console.log(result2.timestamp);
+        console.log(parseInt(result1.timestamp)-parseInt(result2.timestamp));
+        var blocktime = parseInt(result1.timestamp)-parseInt(result2.timestamp);
+
+        count++;
+        sum = sum + blocktime;
+        console.log("AvE=" + sum/count);
+
+
+        var xx ={
+            _id: result1.number,
+            difficulty:result1.difficulty.toString(),
+            miner:result1.miner,
+            timestamp:result1.timestamp,
+            totalDifficulty:result1.totalDifficulty.toString(),
+            blocktime:blocktime
+
+        };
+
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("test1");
+            dbo.collection("site8").insertOne(xx, function(err, res) {
+                if (err){
+                    console.log("Repeat");
+                }
+                console.log("文档插入成功");
+                db.close();
+
+            });
+        });
+
+
+
+    }
+
+
+}
+
+main();
